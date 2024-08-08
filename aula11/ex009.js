17d48eaf765f852a2972791fa312980376e2fbff
daianesf <156373089+daianesf@users.noreply.github.com>
2024-08-08 11:52:57 -0300

    Aulas 10 e 12

---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = 'Italia'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil') {
+    console.log('Você é brasileiro')
+} else {
+    console.log("Você é estrangeiro!")}
---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 16
+if(idade >= 18){
+    console.log(`vota`)
+} else{
+    if(idade >= 16 && idade < 18){
+        console.log(`voto opcional`)
+    }else{
+        console.log (`Não vota`)
+    }
+}
